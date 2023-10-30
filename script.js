b45199f5613d6b5c62acb4c8de401a65dcd500be
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2023-10-30 10:29:24 -0300

    Atualizações em diversas matérias, devido troca de professores e ajustes para melhor compatibilidade de agendas

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const materias = [
         linkName: "UPX II"
     },
     {
-        materia: "Programação Orientada à Objetos",
-        professor: "Em processo de contratação.",
-        email: "primeiro_nome.último_nome@facens.br",
+        materia: "Banco de Dados",
+        professor: "Vanessa Cristina Lourenço",
+        email: "vanessa.lourenco@facens.br",
         dia: "Quarta",
-        hora: "20h as 21h",
-        link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmQzNzViMjEtOTk2Yi00NDRlLWJkNGItNDRjY2RkYzc1ZWQ4%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%22adfd45bf-a88e-43ba-a7c8-0ac7e35a4fd7%22%7d",
-        linkName: "POO"
+        hora: "21:30h as 22:30h",
+        link: "https://teams.microsoft.com/l/meetup-join/19%3Ameeting_ZTg5NTM1NjMtN2QxZi00N2QyLWJhNzYtMWY3MGYxYmM1Y2Q4%40thread.v2/0?context=%7B%22Tid%22%3A%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2C%22Oid%22%3A%22e8ed8b30-a1ab-4480-b76d-cc355287fb38%22%2C%22MessageId%22%3A%220%22%7D",
+        linkName: "BD"
     },
     {
         materia: "Engenharia de Software",
@@ -30,8 +30,8 @@ const materias = [
     },
     {
         materia: "Redes de Computadores",
-        professor: "Eliney Sabino",
-        email: "eliney.sabino@facens.br",
+        professor: "Amauri Antunes Filho",
+        email: "amauri.filho@facens.br",
         dia: "Quinta",
         hora: "20h as 21h",
         link: "https://teams.microsoft.com/l/meetup-join/19%3Ameeting_MjJiZDAwZWUtNTEwNi00YTk1LTk3YTUtNDk0NWNmM2I5Mjdh%40thread.v2/0?context=%7B%22Tid%22%3A%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2C%22Oid%22%3A%22e8ed8b30-a1ab-4480-b76d-cc355287fb38%22%2C%22MessageId%22%3A%220%22%7D",
@@ -47,13 +47,13 @@ const materias = [
         linkName: "Des. Hum. e Social"
     },
     {
-        materia: "Banco de Dados",
-        professor: "Em processo de contratação.",
-        email: "primeiro_nome.último_nome@facens.br",
+        materia: "Programação Orientada à Objetos",
+        professor: "Emerson Dos Santos Paduan",
+        email: "emerson.paduan@facens.br",
         dia: "Sexta",
-        hora: "19h as 20h",
-        link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTY1YzFlNmMtYmEzOC00NTlhLTkzZDYtNDFjMDA3YWZlNDEy%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%22adfd45bf-a88e-43ba-a7c8-0ac7e35a4fd7%22%7d",
-        linkName: "BD"
+        hora: "21h as 22h",
+        link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmQzNzViMjEtOTk2Yi00NDRlLWJkNGItNDRjY2RkYzc1ZWQ4%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%22adfd45bf-a88e-43ba-a7c8-0ac7e35a4fd7%22%7d",
+        linkName: "POO"
     }
 ]
 
@@ -75,5 +75,3 @@ for(let i = 0; i < materias.length; i++){
 
     materias[i].link == "Não divulgado" ? td_link.innerText = "Ainda não está disponível" : td_link.innerHTML = `<a href="${materias[i].link}">Clique para acessar o chat de ${materias[i].linkName}</a>`
 }
-
-alert("Ainda não sabemos como serão as matérias de POO e BD, devido a saída da professora Maria.\nAssim que divulgado será alterado aqui!\nClique em OK para visualizar as matéria que estão corretas.")
