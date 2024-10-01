d27e08ac71aaed3f8411f5098d592eb735532d6a
Tiago Macedo Cardoso <86525944+TiagoMacedoso@users.noreply.github.com>
2024-10-01 20:45:02 -0300

    Atualização em quase todas as matérias devido a uma troca de professores.

--- script.js
+++ script.js
@@ -11,22 +11,40 @@ const materias = [
         linkName: "UPX IV"
     },
     {
+        materia: "Qualidade e Teste de Software",
+        professor: "Marcelo da Silva",
+        email: "marcelo.silva@facens.br",
+        dia: "Segunda",
+        hora: "21:50h as 22:50h",
+        link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_N2ExNWRiODktODk2OC00NWZiLWFjYTUtOGZmYzI4ZGRlOGMx%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%22bbfbe01a-312d-4822-81f9-33b52731d6da%22%7d",
+        linkName: "QTS"
+    },
+    {
+        materia: "Business Intelligence",
+        professor: "Marcelo da Silva",
+        email: "marcelo.silva@facens.br",
+        dia: "Terça",
+        hora: "20h as 21h",
+        link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZjVmMTg5NTctODM5MC00ZWY2LTg0MjMtOTJhZjYwYzE5ZDNm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%252259d4f249-0520-46cf-b6b2-873d5da543fe%2522%252c%2522Oid%2522%253a%252291b2b98c-37cc-46eb-aec4-4c28b34364f4%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=91610a15-9391-47ee-8582-30a2895dac19&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true",
+        linkName: "BI"
+    },
+    {
+        materia: "Eletiva 1",
+        professor: "Marcelo da Silva",
+        email: "marcelo.silva@facens.br",
+        dia: "Terça",
+        hora: "20h as 21h",
+        link: "https://facens.br",
+        linkName: "Eletiva"
+    },
+    {
         materia: "Modelagem de Processos de Negócios",
         professor: "Paula Donaduzzi Rigo",
         email: "paula.rigo@facens.br",
-        dia: "Terça",
+        dia: "Quarta",
         hora: "20h as 21h",
         link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZDVhNDY3YTUtODA0ZC00NDVmLWE2OWUtNjcxYWUzYzNmMjYw%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%252259d4f249-0520-46cf-b6b2-873d5da543fe%2522%252c%2522Oid%2522%253a%2522fea352f5-65e2-4830-bbf8-57420354406f%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=ee19771f-70a5-4f81-9bf8-0fe421a17955&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true",
         linkName: "MPN"
-    },
-    {
-        materia: "Qualidade e Teste de Software",
-        professor: "Rafael Albuquerque Pinto",
-        email: "rafael.pinto@facens.br",
-        dia: "Quarta",
-        hora: "21:50h as 22:40h",
-        link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_N2ExNWRiODktODk2OC00NWZiLWFjYTUtOGZmYzI4ZGRlOGMx%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%22bbfbe01a-312d-4822-81f9-33b52731d6da%22%7d",
-        linkName: "QTS"
     },
     {
         materia: "Programação Web",
@@ -36,24 +54,6 @@ const materias = [
         hora: "21h as 22h",
         link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_YWI2NDUxYTMtYjRjYi00ZDM2LWFhYTQtOWI0OTkwMWM2YmI0%40thread.v2/0?context=%7b%22Tid%22%3a%2259d4f249-0520-46cf-b6b2-873d5da543fe%22%2c%22Oid%22%3a%224ecc2cd5-c3b7-451d-9613-63bc411f662b%22%7d",
         linkName: "PW"
-    },
-    {
-        materia: "Business Intelligence",
-        professor: "SEM PROFESSOR",
-        email: ".@facens.br",
-        dia: "-",
-        hora: "-h as -h",
-        link: "https://facens.br",
-        linkName: "BI"
-    },
-    {
-        materia: "Eletiva 1",
-        professor: "SEM PROFESSOR",
-        email: ".@facens.br",
-        dia: "-",
-        hora: "-h as -h",
-        link: "https://facens.br",
-        linkName: "Eletiva"
     }
 ]
 
